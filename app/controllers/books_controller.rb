040f68abc3f8a7c90abf4244c55621b6df361325
vagrant <vagrant@localhost.localdomain>
2020-07-21 11:56:43 +0000

    [finish]bk2-before_debug

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,22 +1,19 @@
 class BooksController < ApplicationController
   before_action :authenticate_user!
+  before_action :ensure_current_user, {only: [:edit, :update, :destroy]}
+
 
   def show
+    @newBook = Book.new
     @book = Book.find(params[:id])
+    @user = current_user
+    @user_show = @book.user
   end
 
   def index
     @books = Book.all
-  end
-
-  def create
-    @book = Book.new(book_params)
-    if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
-    else
-      @books = Book.all
-      render 'index'
-    end
+    @user = current_user
+    @newBook = Book.new
   end
 
   def edit
@@ -24,17 +21,30 @@ class BooksController < ApplicationController
   end
 
 
+  def create
+        @newBook = Book.new(book_params)
+        @newBook.user_id = current_user.id
+    if  @newBook.save
+        redirect_to book_path(@newBook), notice: "You have created book successfully."
+    else
+        @books = Book.all
+        @user = current_user
+        render 'index'
+    end
+  end
+
 
   def update
-    @book = Book.find(params[:id])
-    if @book.update(book_params)
-      redirect_to book_path(@book), notice: "You have updated book successfully."
+        @book = Book.find(params[:id])
+        @books = Book.all
+    if  @book.update(book_params)
+        redirect_to book_path(@book), notice: "You have updated book successfully."
     else
       render "edit"
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
     @book.destoy
     redirect_to books_path
@@ -42,8 +52,15 @@ class BooksController < ApplicationController
 
   private
 
+  def   ensure_current_user
+        @book = Book.find(params[:id])
+    if  @book.user_id != current_user.id
+        redirect_to books_path
+    end
+  end
+
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,16 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :ensure_correct_user, only: [:update]
+  before_action :ensure_correct_user, {only: [:edit, :update]}
 
   def show
     @user = User.find(params[:id])
     @books = @user.books
-    @book = Book.new
+    @newBook = Book.new
   end
 
   def index
     @users = User.all
-    @book = Book.new
+    @newBook = Book.new
   end
 
   def edit
@@ -18,10 +18,11 @@ class UsersController < ApplicationController
   end
 
   def update
+      @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+      redirect_to user_path(@user), notice: "You have updated user successfully."
     else
-      render "show"
+      render "edit"
     end
   end
 
